282fd4cd1ce9bf83a8331efbc302d441e2b2295f
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-05-27 20:48:56 +0900

    feat - 수동 구매 갯수 및 수동 번호 입력 Input 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -15,12 +15,25 @@ class InputView {
 
         fun inputWinnerNumbers(): String {
             println("지난 주 당첨 번호를 입력해 주세요.")
-            return sc.next()
+            return inputStringScanner()
+        }
+        fun inputManualBuyCount(): Int {
+            println("수동으로 구매할 로또 수를 입력해 주세요.")
+            return inputIntScanner()
+        }
+
+        fun inputManualNumbers(): String {
+            println("수동으로 구매할 번호를 입력해 주세요.")
+            return inputStringScanner()
         }
 
         fun inputBonusBall(): Int {
             println("보너스 볼을 입력해 주세요.")
             return inputIntScanner()
+        }
+
+        private fun inputStringScanner(): String {
+            return sc.next()
         }
 
         private fun inputIntScanner() = sc.nextInt()
